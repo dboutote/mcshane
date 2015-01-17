37713963e12a6a627845c3bf4d9cc7ac4cc2c69f
dboutote <dboutote@gmail.com>
2015-01-17 00:04:37 -0600

    added classes to feat. link anchors

--- application/wp-content/mu-plugins/custom-post-types/class_cpt_flink.php
+++ application/wp-content/mu-plugins/custom-post-types/class_cpt_flink.php
@@ -95,8 +95,8 @@ class CPT_FeatLink
 				<div class="feature-link clearfix">
 					<div class="left">
 						<?php if($_flink_url) { ?>
-							<a href="<?php echo esc_url($_flink_url);?>"><img src="<?php echo get_stylesheet_directory_uri();?>/images/arrow.svg"></a>
-							<div class="title"><a href="<?php echo esc_url($_flink_url);?>"><?php echo apply_filters('the_title', $post_object->post_title);?></a></div>
+							<a class="link-arrow" href="<?php echo esc_url($_flink_url);?>"><img src="<?php echo get_stylesheet_directory_uri();?>/images/arrow.svg"></a>
+							<div class="title"><a class="link-title" href="<?php echo esc_url($_flink_url);?>"><?php echo apply_filters('the_title', $post_object->post_title);?></a></div>
 							<div class="content"><?php echo $post_object->post_excerpt;?></div>
 						<?php }; ?>
 					</div>
@@ -107,7 +107,7 @@ class CPT_FeatLink
 						$img_width = $image_obj[1];
 						$img_height = $image_obj[2]; ?>
 						<div class="right">
-							<a href="<?php echo esc_url($_flink_url);?>">
+							<a class="link-img" href="<?php echo esc_url($_flink_url);?>">
 								<img src="<?php echo $img_src;?>" width="<?php echo $img_width;?>" height="<?php echo $img_height;?>" class="background-cover" alt="" />
 							</a>
 						</div>
